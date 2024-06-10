834dd1367d692cde8d9f9d4b092d1d146cac7480
alfredochola <ochalfie@gmail.com>
2024-06-10 16:06:19 +0300

    popup

--- _site/assets/js/sponsor.js
+++ _site/assets/js/sponsor.js
@@ -1,9 +1,9 @@
 Swal.fire({
   title: "Sponsor a child's Education?",
   html: `
-  <h1 class="text-center display-1 "> <span class="bi bi-pen-fill text-dark"></span></h1>    
+  <h1 class="text-center display-1 "> <span class="bi bi-pen-fill text-warning"></span></h1>    
   Would you like to sponsor a child?
-  <p><a class="btn btn-dark mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-success m-2" onclick="contact()">Yes please</a></p>
+  <p><a class="btn btn-light mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-warning m-2" onclick="contact()">Yes please</a></p>
      
     `,
   showCloseButton: true,
