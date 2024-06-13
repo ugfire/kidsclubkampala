a58be9f229e551a3e471e82c0f697b54a816e249
alfredochola <ochalfie@gmail.com>
2024-06-13 10:23:57 +0300

    added projects

---
+++ assets/js/projects.js
@@ -0,0 +1,30 @@
+function ewafe() {
+  const ebtn = document.getElementById("ewafebtn");
+  const ewafedesc = document.getElementById("ewafe");
+  ebtn.style.display = "none";
+  ewafedesc.style.display = "block";
+}
+function family() {
+  const fbtn = document.getElementById("familybtn");
+  const familydesc = document.getElementById("family");
+  fbtn.style.display = "none";
+  familydesc.style.display = "block";
+  const fpart = document.getElementById("fpart");
+  fpart.style.display = "block";
+}
+function protection() {
+  const pbtn = document.getElementById("pbtn");
+  const pdesc = document.getElementById("protection");
+  pbtn.style.display = "none";
+  pdesc.style.display = "block";
+  const ppart = document.getElementById("ppart");
+  ppart.style.display = "block";
+}
+function education() {
+  const edubtn = document.getElementById("educbtn");
+  const edesc = document.getElementById("education");
+  edubtn.style.display = "none";
+  edesc.style.display = "block";
+  const epart = document.getElementById("epart");
+  epart.style.display = "block";
+}
