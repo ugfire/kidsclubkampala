3389aeb43bbdd1fa05f434a2ce31dcfed1aaaac3
ugfire <ugfirecompany@gmail.com>
2024-06-19 14:24:08 +0300

    added removed apply

--- assets/js/apply.js
+++ assets/js/apply.js
@@ -5,6 +5,21 @@ function apply() {
         <div class="">
         <h1 class="text-center display-1 "> <span class="bi bi-computer text-danger"></span></h1>    
        <h2>Apply Now</h2>
+       <form  method="post" class="applicationform">
+  <p class="my-1">
+    <input type="text" name="name" class="form-control" placeholder="Your Name " id="" required/>
+  </p>
+  <p class="my-1">
+    <input type="text" name="email" class="form-control"  placeholder="Your Email " id="" required/>
+  </p>
+  <p class="my-1">
+    <input type="text" name="phone" class="form-control"  placeholder="Your Phone " id="" required/>
+  </p>
+  <p class="my-1">
+    <textarea type="text" class="form-control"  name="message" placeholder="Your Message " id="" required/></textarea>
+  </p>
+</form>
+
         <p><a class="btn btn-dark mr-2 m-2" onclick="Swal.close()">Cancel</a><a class="btn btn-warning m-2" onclick="sendapplication()">Send</a></p>
          
         </div>  
@@ -21,5 +36,9 @@ function close() {
 }
 
 function sendapplication() {
+  const name = document.getElementsByClassName("name");
+  const email = document.getElementsByClassName("email");
+  const phone = document.getElementsByClassName("phone");
+  const message = document.getElementsByClassName("message");
   alert("application sent successfully !");
 }
