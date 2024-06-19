87c9a291fa40aebb034bba9e8159f52a57bc3fa8
ugfire <ugfirecompany@gmail.com>
2024-06-19 08:32:37 +0300

    added job

---
+++ assets/js/apply.js
@@ -0,0 +1,22 @@
+function apply() {
+  Swal.fire({
+    title: "Apply for a job",
+    html: `
+        <h1 class="text-center display-1 "> <span class="bi bi-pen-fill text-warning"></span></h1>    
+        Would you like to sponsor a child?
+        <p><a class="btn btn-light mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-warning m-2" onclick="contact()">Yes please</a></p>
+           
+          `,
+    showCloseButton: true,
+    showCancelButton: false,
+    showConfirmButton: false,
+  });
+}
+
+function close() {
+  Swal.close();
+}
+
+function contact() {
+  window.location.href = "/contact";
+}
