78e54da27a12c1ab1649b78aef7e480f088f537f
ugfire <ugfirecompany@gmail.com>
2024-06-24 15:01:16 +0300

    some changes

--- assets/js/sponsor.js
+++ assets/js/sponsor.js
@@ -1,8 +1,8 @@
 Swal.fire({
-  title: "Sponsor a child's Education?",
+  title: " Foster/adopt a child?",
   html: `
   <h1 class="text-center display-1 "> <span class="bi bi-pen-fill text-warning"></span></h1>    
-  Would you like to sponsor a child?
+ 
   <p><a class="btn btn-light mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-warning m-2" onclick="contact()">Yes please</a></p>
      
     `,
